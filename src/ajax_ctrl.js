63a2c33db77f1894d53bc21c82efe19c5f43c11b
Ryan McKinley <ryantxu@gmail.com>
2016-09-02 10:56:52 -0700

    cleanup

--- src/ajax_ctrl.js
+++ src/ajax_ctrl.js
@@ -9,12 +9,16 @@ const panelDefaults = {
   method: 'GET',
   url: 'https://raw.githubusercontent.com/ryantxu/ajax-panel/master/static/example.txt',
   errorMode: 'show',
-  params_js:"{\nfrom:ctrl.range.from.format('x'),   // x is unix ms timestamp\nto:ctrl.range.to.format('x'),\nheight:ctrl.height\n}",
+  params_js: "{\n"+
+             " from:ctrl.range.from.format('x'),  // x is unix ms timestamp\n"+
+             " to:ctrl.range.to.format('x'), \n"+
+             " height:ctrl.height\n"
+             "}",
   display_js: null
 };
 
 export class AjaxCtrl extends MetricsPanelCtrl {
- // constructor($scope, $injector, private templateSrv, private $sce) { 
+  // constructor($scope, $injector, private templateSrv, private $sce) { 
   constructor($scope, $injector, templateSrv, $sce, $http) {
     super($scope, $injector);
     this.$sce = $sce;
@@ -26,17 +30,16 @@ export class AjaxCtrl extends MetricsPanelCtrl {
 
     this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
     this.events.on('panel-initialized', this.onPanelInitalized.bind(this));
-    this.events.on('refresh', this.onRender.bind(this));
+    this.events.on('refresh', this.onRefresh.bind(this));
     this.events.on('render', this.onRender.bind(this));
   }
 
-
+  // This just skips trying to send the actual query.  perhaps there is a better way
   issueQueries(datasource) {
     this.updateTimeRange();
 
     console.log('block issueQueries', datasource);
   }
-
 
   onPanelInitalized() {
     this.updateFN();
@@ -52,67 +55,64 @@ export class AjaxCtrl extends MetricsPanelCtrl {
 
   onPanelTeardown() {
    // this.$timeout.cancel(this.nextTickPromise);
-  }
+  } 
 
   updateFN() {
     this.params_fn = null;
     this.display_fn = null;
 
     if(this.panel.params_js) {
-    try {
-      this.params_fn = new Function('ctrl', 'return ' + this.panel.params_js);
+      try {
+        this.params_fn = new Function('ctrl', 'return ' + this.panel.params_js);
+      }
+      catch( ex ) {
+        console.warn('error parsing params_js', this.panel.params_js, ex );
+        this.params_fn = null;
+      }
     }
-    catch( ex ) {
-      console.log('error parsing params_js', this.panel.params_js, ex );
-      this.params_fn = null;
+    if(this.panel.display_js) {
+      try {
+        this.display_fn = new Function('ctrl', 'response', this.panel.display_js);
+      }
+      catch( ex ) {
+        console.warn('error parsing display_js', this.panel.display_js, ex );
+        this.display_fn = null;
+      }
     }
-}
-if(this.panel.display_js) {
-    try {
-      this.display_fn = new Function('ctrl', 'response', this.panel.display_js);
-    }
-    catch( ex ) {
-      console.log('error parsing display_js', this.panel.display_js, ex );
-      this.display_fn = null;
-    }
-}
-    this.render();
+
+    this.onRefresh();
   }
 
-  onRender() {
+  onRefresh() {
+    console.log('refresh', this);
+    this.updateTimeRange();
 
-console.log('render', this);
-  this.updateTimeRange();
 
-
-(function(wrap){ // need access to 'this' later :(
-    var params;
-    if(wrap.params_fn) {
-      params = wrap.params_fn( wrap );
-    }
-    console.log( "onRender", wrap, params );
-    
-    wrap.$http({
-      method: wrap.panel.method,
-      url: wrap.panel.url,
-      params: params
-    }).then(function successCallback(response) {
-      console.log('success', response, wrap);
-      var html = response.data;
-      if(wrap.display_fn) {
-	console.log('before display xform', response, wrap.display_fn);
-        html = wrap.display_fn(wrap, response);
-	console.log('after display xform', html);
+    (function(wrap){ // Must be a better way!  maybe the http callbacks as function in this class?
+      var params;
+      if(wrap.params_fn) {
+        params = wrap.params_fn( wrap );
       }
-      wrap.updateContent( html );
-    }, function errorCallback(response) {
-      console.log('error', response);
-      var body = '<h1>Error</h1><pre>' + JSON.stringify(response, null, " ") + "</pre>";
-      wrap.updateContent(body);
-    });
+      console.log( "onRender", wrap, params );
+      
+      wrap.$http({
+        method: wrap.panel.method,
+        url: wrap.panel.url,
+        params: params
+      }).then(function successCallback(response) {
+        console.log('success', response, wrap);
+        var html = response.data;
+        if(wrap.display_fn) {
+          html = wrap.display_fn(wrap, response);
+        }
+        wrap.updateContent( html );
+      }, function errorCallback(response) {
+        console.log('error', response);
+        var body = '<h1>Error</h1><pre>' + JSON.stringify(response, null, " ") + "</pre>";
+        wrap.updateContent(body);
+      });
 
-}(this));
-
+    }(this));
   }
 
   updateContent(html) {
@@ -122,6 +122,10 @@ console.log('render', this);
       console.log('Text panel error: ', e);
       this.content = this.$sce.trustAsHtml(html);
     }
+  }
+
+  onRender() {
+    console.log('render', this);
   }
 }
 

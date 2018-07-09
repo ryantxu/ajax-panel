1e18b8c38e05818f9d920d0f4482aaff85005f38
Ryan McKinley <ryantxu@gmail.com>
2018-07-09 17:07:42 +0200

    adding a release script

--- prepare-release.js
+++ prepare-release.js
@@ -1,16 +1,35 @@
-var fs = require('fs');
-var versionDev = require('./package.json').version;
+const fs = require('fs');
+const versionDev = require('./package.json').version;
 if (!versionDev.endsWith('-dev')) {
   console.warn('This script only works with a -dev version.  Not: ' + versionDev);
   process.exit(1);
 }
-var version = versionDev.substring(0, versionDev.lastIndexOf('-'));
-exec('git checkout -b release-' + version, function(err, stdout, stderr) {
-  if (err) {
-    console.warn('Error running git checkout: ', err);
-    process.exit(1);
-  }
-  console.log(stdout);
-});
+
+// Check if the local branch has any changes not commited to git
+const version = versionDev.substring(0, versionDev.lastIndexOf('-'));
+const {execSync} = require('child_process');
+
+const gitStatus = execSync('git status --untracked-files=no --porcelain');
+if (gitStatus.length > 0) {
+  console.warn('Make sure to commit all files before running this script:\n' + gitStatus);
+  process.exit(1);
+}
+
+// exec('git ', function(err, stdout, stderr) {
+//   if (err) {
+//     console.warn('Error running git status: ', err);
+//     process.exit(1);
+//   }
+//   console.log('[STATUS]', stdout);
+// });
+
+// // Make a branch with the current release name
+// exec('git checkout -b release-' + version, function(err, stdout, stderr) {
+//   if (err) {
+//     console.warn('Error running git checkout: ', err);
+//     process.exit(1);
+//   }
+//   console.log('[BRANCH]', stdout);
+// });
 
 console.log('prepare release: ', version);

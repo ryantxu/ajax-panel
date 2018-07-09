dfc0ee6e1b5d05c627de44b0cead88bf098b7248
Ryan McKinley <ryantxu@gmail.com>
2018-07-09 17:29:15 +0200

    more release script

--- prepare-release.js
+++ prepare-release.js
@@ -9,27 +9,44 @@ if (!versionDev.endsWith('-dev')) {
 const version = versionDev.substring(0, versionDev.lastIndexOf('-'));
 const {execSync} = require('child_process');
 
-const gitStatus = execSync('git status --untracked-files=no --porcelain');
-if (gitStatus.length > 0) {
-  console.warn('Make sure to commit all files before running this script:\n' + gitStatus);
+let output = execSync('git status --untracked-files=no --porcelain');
+if (false && output.length > 0) {
+  console.warn('Make sure to commit all files before running this script:\n' + output);
   process.exit(1);
 }
 
-// exec('git ', function(err, stdout, stderr) {
-//   if (err) {
-//     console.warn('Error running git status: ', err);
-//     process.exit(1);
-//   }
-//   console.log('[STATUS]', stdout);
-// });
+console.log('Checkout and publish release branch');
+execSync('git checkout -b release-' + version);
+execSync("git commit -a -m 'adding a release branch'");
 
-// // Make a branch with the current release name
-// exec('git checkout -b release-' + version, function(err, stdout, stderr) {
-//   if (err) {
-//     console.warn('Error running git checkout: ', err);
-//     process.exit(1);
-//   }
-//   console.log('[BRANCH]', stdout);
-// });
+function searchReplaceFile(regexpFind, replace, theFileName) {
+  var file = fs.createReadStream(theFileName, 'utf8');
+  var newText = '';
+
+  file.on('data', function(chunk) {
+    newText += chunk.toString().replace(regexpFind, replace);
+  });
+
+  file.on('end', function() {
+    fs.writeFile(theFileName, newText, function(err) {
+      if (err) {
+        return console.warn(err);
+      }
+    });
+  });
+}
+
+console.log('Replace in files');
+searchReplaceFile(versionDev, version, 'package.json');
+searchReplaceFile('dist/', '', '.gitignore');
+
+// console.log('Test');
+// execSync('yarn test');
+
+console.log('Building...');
+execSync('yarn build');
+
+console.log('Save the artifacts in git');
+execSync("git commit -a -m 'adding artifacts'");
 
 console.log('prepare release: ', version);
